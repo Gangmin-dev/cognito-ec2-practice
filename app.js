4fbac535264bebcf6034b6d197247820e9dc93cd
Gangmin-dev <vinu10045@snu.ac.kr>
2021-05-27 18:03:33 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const loginRouter = require("./routes/login");
+
+const port = 8426;
+const app = express();
+
+app.use("/login", loginRouter);
+
+app.use((req, res, next) => {
+  res.status(404).send("Not Found");
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send("error \n", err.message);
+});
+
+app.listen(port, () => {
+  let a = {
+    email: "aa@aa.com",
+    phoneNumber: "+8201055645274",
+  };
+
+  console.log(Object.entries(a));
+  console.log(`Run Server with port number ${port}`);
+});
